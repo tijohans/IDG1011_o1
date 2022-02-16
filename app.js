77e769e320584a449ca37d263073f50d23bd7816
tijohans <tijohans@stud.ntnu.no>
2022-02-16 19:25:29 +0100

    Made getRandomQuote() to work

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ const quotes = {
         "I don't need life I'm high on drugs"
     ]
 }
+
+
+const getRandomQuote = _ => {
+    // Gets the different keys from quotes in an array
+    const keys = Object.keys(quotes);
+
+    // Gets a random key from the keys array, using Math.floor and Math.random times the length of the array
+    const randomKey = keys[Math.floor(Math.random() * keys.length)];
+
+    // Gets a random index from the length of the value in the array of the random key
+    const randomIndex = Math.floor(Math.random()* quotes[randomKey].length);
+
+    // Returns a random quote from a random index in an array from a random key
+    return quotes[randomKey][randomIndex];
+}
+
+const quote = getRandomQuote();
